0a2685185d60cb276465eb45728c68f54152f281
kannans <kannanhudson@gmail.com>
2014-05-01 16:52:10 +0530

    Post details page * Post details page added separatly. * title and body values displied in the page.

--- app/assets/javascripts/routes/posts.js
+++ app/assets/javascripts/routes/posts.js
@@ -1,5 +1,13 @@
+// Action:index
 CookingRecipes.PostsRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('post');
+  }
+});
+
+// Action:show
+CookingRecipes.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('post', params.id);
   }
 });
