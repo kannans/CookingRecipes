827e17fb24152c32428f20c86b015196a43dc57e
kannans <kannanhudson@gmail.com>
2014-05-01 18:58:17 +0530

    Delete post from details page * Delete button and function added in the post details page

--- app/assets/javascripts/controllers/posts.js
+++ app/assets/javascripts/controllers/posts.js
@@ -17,3 +17,17 @@ CookingRecipes.PostsNewController = Ember.ObjectController.extend({
     }
   }
 });
+
+CookingRecipes.PostController = Ember.ObjectController.extend({
+  actions: {
+    remove: function(site) {
+      var self = this;
+      if (window.confirm("Are you sure you want to delete this?")) {
+        site.deleteRecord();
+        site.save().then(function() {
+          self.transitionToRoute('index');
+        });
+      }
+    }
+  }
+});
